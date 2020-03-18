ef3bd219cfaec03a3a9a065b7c8e64f885b23b2a
Aaron <aglenn@laureateinstitute.org>
2020-03-18 12:11:26 -0500

    init

---
+++ Get-VMOSInfo.ps1
@@ -0,0 +1,10 @@
+﻿. $PSScriptRoot\Initialize-PowerCLI
+
+$AllVMs=Get-VM
+$VMsWithOS=$AllVMs|Select Name,@{Name="OS";Expression={$_.ExtensionData.Guest.GuestFullName}}|Sort -property OS,Name
+$VMsWithOS|ft
+$VMsWithOS|Export-Csv "$PSScriptRoot\VMsWithOS.csv" -NoTypeInformation
+"Output saved to $PSScriptRoot\VMsWithOS.csv"
+
+
+
---
+++ Initialize-PowerCLI.ps1
@@ -0,0 +1,18 @@
+﻿if (!(Get-installedmodule vmware* -ErrorAction SilentlyContinue)){ 
+    Install-Module –Name VMware.PowerCLI -AllowClobber -Scope CurrentUser
+}
+
+if (Get-module *vmware*) { write-verbose "VM Modules already loaded"}
+else { 
+    Import-Module Vmware.PowerCLI
+}
+Set-PowerCLIConfiguration -InvalidCertificateAction Ignore -Scope Session -Confirm:$false |out-null
+
+$VCSA=Read-Host -Prompt 'FQDN of vcenter host'
+
+try {Get-ViRole -ErrorAction SilentlyContinue |Out-Null }
+catch {
+    Write-Host "** Connecting to $VCSA " -ForegroundColor Yellow
+    Connect-VIServer -server $VCSA -Protocol https -InformationAction SilentlyContinue |out-null
+    Write-Host '** Complete' -ForegroundColor Cyan
+    }
