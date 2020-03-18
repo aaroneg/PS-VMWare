2dbac4c8704c0ff4fac8f0da28cf0fad521ae115
Aaron <aglenn@laureateinstitute.org>
2020-03-18 12:14:01 -0500

    eject cds

---
+++ Disconnect-AllCDDrives.ps1
@@ -0,0 +1,4 @@
+﻿. $PSScriptRoot\Initialize-PowerCLI
+$AllVMS = Get-VM | Select-Object Name, @{Label = "ISOfile"; Expression = { ($_ | Get-CDDrive).ISOPath } }
+$AllVMs | Where-Object { $_.ISOfile }
+$AllVMs | Where-Object { $_.ISOfile } | ForEach-Object { Get-vm $_.Name | Get-CDDrive | Set-CDDrive -NoMedia -confirm:$false }
